5f78763652840f8285af5aa009a9d5c4dea1ad77
raylu <90059+raylu@users.noreply.github.com>
2024-07-18 19:59:09 -0700

    add root_dir to path

--- packaging/pypi.py
+++ packaging/pypi.py
@@ -2,6 +2,7 @@
 
 import os
 import pathlib
+import sys
 
 root_dir = pathlib.Path(__file__).parents[1]
 dool_dir = root_dir / 'dool'
@@ -10,6 +11,7 @@ dool_dir.mkdir()
 (root_dir / '__main__.py').rename(dool_dir / '__main__.py')
 (root_dir / 'plugins').rename(dool_dir / 'plugins')
 
+sys.path.append(root_dir.as_posix())
 import dool.__main__
 
 (dool_dir / '__init__.py').write_text(f'"""{dool.__main__.__doc__}"""\n\n__version__ = "{dool.__main__.__version__}"')
